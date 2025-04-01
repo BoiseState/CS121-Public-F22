3969c0ae132627185b46b32dbac05380b73ec3a2
MasonVail <masonvail@boisestate.edu>
2025-04-01 14:30:54 -0600

    removed unnecessary import

--- examples/module6/FileWriter.java
+++ examples/module6/FileWriter.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
