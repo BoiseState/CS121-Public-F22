13d468ba661fcbcda4fd2c5d4699122add422d52
MasonVail <masonvail@boisestate.edu>
2025-04-22 07:50:52 -0600

    push counter with comments

--- examples/module8/PushCounter.java
+++ examples/module8/PushCounter.java
@@ -1,26 +1,28 @@
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
-/**
- * Demonstrates a graphical user interface and an event listener.
- * 
+/** Demonstrates a graphical user interface and an event listener.
  * @author Java Foundations
  */
 public class PushCounter
 {
-   /**
-    * Creates and displays the main program frame.
-    * @param args
+   /** Creates and displays the main program frame.
+    * @param args unused
     */
    public static void main(String[] args)
    {
+	  //create the top-level frame to hold the GUI
+	  //and coordinate with the operating system
       JFrame frame = new JFrame("Push Counter");
+      //specify what the GUI should do when the close button
+      //is pressed - chance to save info before exiting
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+      
+      //create the top-level panel to hold all content
       PushCounterPanel panel = new PushCounterPanel();
+      //add the panel to the frame
       frame.getContentPane().add(panel);
 
+      //prepare the GUI for display and make it visible
       frame.pack();
       frame.setVisible(true);
    }
--- examples/module8/PushCounterPanel.java
+++ examples/module8/PushCounterPanel.java
@@ -1,25 +1,55 @@
+import java.awt.Color;
+import java.awt.Dimension;
+//import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
- * @author amit
- *
+ * Demonstrates a graphical user interface and an event listener.
+ * 
+ * @author Java Foundations
  */
+@SuppressWarnings("serial")
 public class PushCounterPanel extends JPanel {
-	
-	PushButton incButton, decButton;
-	JLabel label;
-	
-	public PushCounterPanel() {
-		label = new JLabel("Pushes: 0");
-		
-		incButton = new PushButton("increment", label);
-		decButton = new PushButton("decrement", label);
-		
-		add(incButton);
-		add(decButton);
-		add(label);
-		
-	}
+   private int count;
+   private JButton push;
+   private JLabel label;
 
+   /** Constructor: Sets up the GUI. */
+   public PushCounterPanel() {
+      count = 0;
+
+      push = new JButton("Push Me!");
+      push.addActionListener(new ButtonListener());
+
+      label = new JLabel("Pushes: " + count);
+
+      this.add(push);
+      this.add(label);
+
+      Color lightGreen = new Color(206, 255, 199);
+      this.setBackground(lightGreen);
+
+      this.setPreferredSize(new Dimension(600, 40));
+   }
+
+   /** Represents a listener for button push (action) events. */
+   private class ButtonListener implements ActionListener {
+      /** Updates the counter and label when the button is pushed. */
+      public void actionPerformed(ActionEvent event) {
+         // System.out.println(event.toString());
+         count++;
+         label.setText("Pushes: " + count);
+         if (push.getText().equals("Push")) {
+            push.setText("Ow!");
+         } else {
+            push.setText("Push");
+         }
+         // push.setEnabled(false);
+      }
+   }
 }
