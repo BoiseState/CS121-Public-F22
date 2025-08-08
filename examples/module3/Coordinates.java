59c1838d88cce02e04ae451cd27a0df2b9c754d2
krodgers <kathrynrodgers@boisestate.edu>
2025-08-07 20:28:25 -0600

    Remove duplicate import

--- examples/module3/Coordinates.java
+++ examples/module3/Coordinates.java
@@ -8,21 +8,20 @@ public class Coordinates
 {
     public static void main(String[] args)
     {
-	// Declare and instantiate object
+        // Declare and instantiate object
         Point origin = new Point(10, 20);
 
-	// Invoke getX method and store result
-      	double x = origin.getX();
+        // Invoke getX method and store result
+        double x = origin.getX();
 
-	// Print the x coordinate
-      	System.out.println("Origin X: " + x);
+        // Print the x coordinate
+        System.out.println("Origin X: " + x);
 
         // Set origin (x,y) to (0,0)
         origin.setLocation(0, 0);
 
         // Get a String describing origin
-	System.out.println(origin.toString()); 
-
+        System.out.println(origin.toString()); 
 
     }
 }
